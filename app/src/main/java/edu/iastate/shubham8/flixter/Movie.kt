15fa811516c8aa9203538e27698b5b3c8c6cda8d
Shubham <bhattacharya.shubham123@gmail.com>
2024-03-17 21:49:19 -0500

    Created basic xml layout and also created some basic kotilin classes

---
+++ app/src/main/java/edu/iastate/shubham8/flixter/Movie.kt
@@ -0,0 +1,7 @@
+package edu.iastate.shubham8.flixter
+
+data class Movie(
+    val title: String,
+    val overview: String,
+    val posterPath: String
+)
---
+++ app/src/main/java/edu/iastate/shubham8/flixter/MovieAdapter.kt
@@ -0,0 +1,16 @@
+package edu.iastate.shubham8.flixter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+//class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+//    // Implement onCreateViewHolder, onBindViewHolder, and getItemCount methods
+//
+//    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+////        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+////        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+////        val overviewTextView: TextView = itemView.findViewById(R.id.overviewTextView)
+//    }
+//}
